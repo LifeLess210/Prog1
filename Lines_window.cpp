2907ab52a7805fc6b690ada46225984982128ee4
LifeLess210 <61515413+LifeLess210@users.noreply.github.com>
2022-03-25 19:14:04 +0100

    Add files via upload
    Ch16 upload, lines_window.cpp update, lines_window.h update

--- Lines_window.cpp
+++ Lines_window.cpp
@@ -1,20 +1,37 @@
 #include "Lines_window.h"
 
-Lines_window::Lines_window(Point xy, int w, int h, const string& title)
-    :Window{xy,w,h,title},
-    next_button{Point{x_max()-300,0}, 150, 40, "Next point",
-        [](Address, Address pw) {reference_to<Lines_window>(pw).next();}},
-    quit_button{Point{x_max()-150,0}, 150, 40, "Quit",
-        [](Address, Address pw) {reference_to<Lines_window>(pw).quit();}},
+Lines_window::Lines_window(Point xy, int w, int h, const string& title):Window{xy,w,h,title},
+    next_button{Point{x_max()-300,0}, 150, 40, "Next point", [](Address, Address pw) {reference_to<Lines_window>(pw).next();}},
+    quit_button{Point{x_max()-150,0}, 150, 40, "Quit", [](Address, Address pw) {reference_to<Lines_window>(pw).quit();}},
     next_x{Point{300,0}, 50, 40, "next x:"},
     next_y{Point{450,0}, 50, 40, "next y:"},
-    xy_out{Point{100,0}, 100, 40, "current (x,y):"}
+    xy_out{Point{100,0}, 100, 40, "current (x,y):"},
+    style_menu{Point{x_max() - 300, 80}, 150, 40,  Menu::vertical, "style"},
+    style_button{Point{x_max() - 300, 80}, 150, 40, "style", [](Address, Address pw) {reference_to<Lines_window>(pw).style_pressed();}},
+    color_menu{Point{x_max() - 150, 80}, 150, 40,  Menu::vertical, "color"},
+    color_button{Point{x_max() - 150, 80}, 150, 40, "color", [](Address, Address pw) {reference_to<Lines_window>(pw).color_pressed();}}
+    
 {
     attach(next_button);
     attach(quit_button);
     attach(next_x);
     attach(next_y);
     attach(xy_out);
+    color_menu.attach(new Button{Point{150, 80},150,40,"red", [](Address, Address pw) {reference_to<Lines_window>(pw).red_pressed();}});
+    color_menu.attach(new Button{Point{150, 120},150,40,"green", [](Address, Address pw) {reference_to<Lines_window>(pw).green_pressed();}});
+    color_menu.attach(new Button{Point{150, 160},150,40,"blue", [](Address, Address pw) {reference_to<Lines_window>(pw).blue_pressed();}});
+    style_menu.attach(new Button{Point{300, 80},150,40,"dotted", [](Address, Address pw) {reference_to<Lines_window>(pw).dotted_pressed();}});
+    style_menu.attach(new Button{Point{300, 120},150,40,"dash", [](Address, Address pw) {reference_to<Lines_window>(pw).dashed_pressed();}});
+    style_menu.attach(new Button{Point{300, 160},150,40,"solid", [](Address, Address pw) {reference_to<Lines_window>(pw).solid_pressed();}});
+    attach(style_menu);
+    attach(color_menu);
+
+    style_menu.hide();
+    color_menu.hide();
+    
+
+    attach(color_button);
+    attach(style_button);
     attach(lines);
 }
 
--- Lines_window.h
+++ Lines_window.h
@@ -17,6 +17,70 @@ private:
 
 	Out_box xy_out;
 
+	Button style_button;
+	Menu style_menu;
+	void change_style(Line_style s)
+	{
+		lines.set_style(s);
+	}
+	void hide_style()
+	{
+		style_menu.hide();
+		style_button.show();
+	}
+	void style_pressed()
+	{
+		style_button.hide();
+		style_menu.show();
+	}
+	void solid_pressed()
+	{
+		change_style(Line_style::solid);
+		hide_style();
+	}
+	void dotted_pressed()
+	{
+		change_style(Line_style::dot);
+		hide_style();
+	}
+	void dashed_pressed()
+	{
+		change_style(Line_style::dash);
+		hide_style();
+	}
+	
+
+	Button color_button;
+	Menu color_menu;
+	void change_color(Color c)
+	{
+		lines.set_color(c);
+	}
+	void hide_color()
+	{
+		color_menu.hide();
+		color_button.show();
+	}
+	void color_pressed()
+	{
+	 	color_button.hide();
+	 	color_menu.show();
+	}
+	void red_pressed()
+	{
+		change_color(Color::red);
+		hide_color();
+	}
+	void green_pressed()
+	{
+		change_color(Color::green);
+		hide_color();
+	}
+	void blue_pressed()
+	{
+		change_color(Color::blue);
+		hide_color();
+	}
 	void next();
 	void quit();
 
